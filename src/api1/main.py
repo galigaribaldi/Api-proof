3fdf2a88745205090fef352b65b272c777fb2144
galigaribaldi <galigaribaldi@live.com>
2021-11-25 13:50:57 -0600

    Endpoints V1

--- src/api1/main.py
+++ src/api1/main.py
@@ -1,8 +1,7 @@
-
 from flask import Flask, json, jsonify, request
 
 ENDPOINT_BASE = "/app1/"
-
+codeStatus = {"error":{"error":404},"success": {"success":201}}
 app = Flask(__name__)
 
 @app.route('/', methods =['GET'])
@@ -10,11 +9,53 @@ app = Flask(__name__)
 def home():
     return "<h1>Home</h1>"
 
-@app.route(ENDPOINT_BASE+"nuevo", methods =['POST'])
-def nuevo():
-    json_entrada = request.json
-    print(json_entrada, type(json_entrada), json_entrada.keys())
-    return jsonify(json_entrada)
+########################################
+############# Endpoints ################
+########################################
 
+@app.route(ENDPOINT_BASE+"registro", methods =['POST'])
+def registro():
+    json_entrada = request.json
+    claves = list(json_entrada.keys())
+    if "RFC" in claves:
+        nombre = json_entrada["nombre"] if "nombre" in claves else ""
+        apellido = json_entrada["apellido"] if "apellido" in claves else ""
+        edad = json_entrada["edad"] if "edad" in claves else ""
+        return jsonify(json_entrada)
+    else:
+        return jsonify(codeStatus["error"])
+    
+@app.route(ENDPOINT_BASE+"consulta", methods =['POST'])
+def consulta():
+    json_entrada = request.json
+    ###Teniendo sólo el RFC
+    if "RFC" in list(json_entrada.keys()):
+        rfc = json_entrada["RFC"]
+        return jsonify(codeStatus["success"])
+    else:
+        return jsonify(codeStatus["error"])
+    
+@app.route(ENDPOINT_BASE+"borrado", methods =['POST'])
+def delete():
+    json_entrada = request.json
+    if "RFC" in list(json_entrada.keys()):
+        rfc = json_entrada["RFC"]
+        return jsonify(codeStatus["success"])
+    else:
+        return jsonify(codeStatus["error"])
+    
+@app.route(ENDPOINT_BASE+"update", methods =['POST'])
+def update():
+    json_entrada = request.json
+    claves = list(json_entrada.keys())
+    if "RFC" in claves:
+        nombre = json_entrada["nombre"] if "nombre" in claves else ""
+        apellido = json_entrada["apellido"] if "apellido" in claves else ""
+        edad = json_entrada["edad"] if "edad" in claves else ""
+        return jsonify(json_entrada)
+        #return jsonify(codeStatus["success"])
+    else:
+        return jsonify(codeStatus["error"])    
+    
 if __name__ == "__main__":
     app.run(debug=True)
